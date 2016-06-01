7320b6729325a51960700efc14f4a3990a30b6d9
Wade Armstrong <wade@wadearmstrong.com>
2016-06-01 10:08:53 -0700

    Almost-entirely-working parser, bad progress bar
    Parser works, but can't test it -- when run through Mocha
    only, hangs without returning.
    
    Progressbar works, but need to split up the Promise.all
    in order to show steps 1-by-1.

--- test/package-parser.js
+++ test/package-parser.js
@@ -2,14 +2,13 @@
 
 const expect = require('expect');
 const fs = require('fs');
-const promise = require('bluebird');
+const sinon = require('sinon');
 
 // File we're testing
 const parser = require('../lib/package-parser.js')
-// let pjKeywords = parser.keywords(targetDeps);
 
 // Load fixtures
-const targetDeps = [ 'mongoose',
+const targetDeps = ['mongoose',
   'redis',
   'express',
   'q',
@@ -19,13 +18,30 @@ const targetDeps = [ 'mongoose',
   'node-solr-smart-client',
   'expect',
   'mocha',
-  'consul'];
-const targetKeywords = ['object relational mapper', 'nodejs', 'orm', 'mssql', 'postgres', 'postgresql', 'sqlite', 'mysql']
+  'consul'
+];
+const targetKeywords = ['object relational mapper', 'nodejs', 'orm', 'mssql', 'postgres', 'postgresql', 'sqlite', 'mysql'];
 const targetDocker = {
-  nginx: "Official build of Nginx.",
-  busybox: "Busybox base image.",
-  ubuntu: "Ubuntu is a Debian-based Linux operating system based on free software.",
-}
+  busybox: 'Busybox base image.',
+  mongo: 'MongoDB document databases provide high availability and easy scalability.',
+  mysql: 'MySQL is a widely used, open-source relational database management system (RDBMS).',
+  nginx: 'Official build of Nginx.',
+  node: 'Node.js is a JavaScript-based platform for server-side and networking applications.',
+  postgres: 'The PostgreSQL object-relational database system provides reliability and data integrity.',
+  redis: 'Redis is an open source key-value store that functions as a data structure server.',
+  registry: 'Containerized docker registry',
+  swarm: 'Swarm: a Docker-native clustering system.',
+  ubuntu: 'Ubuntu is a Debian-based Linux operating system based on free software.'
+};
+const targetFinal = { redis: true,
+  node: true,
+  postgres: true,
+  mysql: true,
+  ghost: true,
+  couchdb: true,
+  solr: true,
+  consul: true
+};
 
 const sampleNPM = fs.readFileSync('./test/fixtures/npm-test.html', 'utf8');
 const sampleDocker = fs.readFileSync('./test/fixtures/docker-test.json', 'utf8');
@@ -35,16 +51,16 @@ const pjDeps = parser.dependencies('./test/fixtures/package-test.json');
 describe('package.json parser', () => {
   // Check that we read all of the packages from package.json (#5)
   it('should grab a list of all packages', () => {
-    expect(pjDeps).toBeAn(Array);
-    expect(pjDeps.length).toEqual(targetDeps.length);
-    expect(pjDeps).toEqual(targetDeps);
+      expect(pjDeps).toBeAn(Array);
+      expect(pjDeps.length).toEqual(targetDeps.length);
+      expect(pjDeps).toEqual(targetDeps);
   });
   it('should generate package URLs', () => {
-  // Do we get proper urls? (#5)
-    let npmURL;
-    for (let dep of targetDeps) {
-      expect(parser.depURL(dep)).toEqual(`https://www.npmjs.com/package/${dep}`);
-    }
+      // Do we get proper urls? (#5)
+      let npmURL;
+      for (let dep of targetDeps) {
+          expect(parser.depURL(dep)).toEqual(`https://www.npmjs.com/package/${dep}`);
+      }
   });
 });
 describe('npmjs.com fetcher', () => {
@@ -53,14 +69,15 @@ describe('npmjs.com fetcher', () => {
     // Hey! This one is synchronous!
     expect(parser.parseDependencies(sampleNPM)).toEqual(targetKeywords);
   });
-  it('should fetch and parse an acutal npmjs.com page', function () {
-  // Do we load a page on npm? (#6)
-  // Use the function () format so that we can pass this
-  this.timeout(6000);
-  return parser.fetchNPM('https://www.npmjs.com/package/sequelize')
-    .then((result) => {
-      expect(result).toEqual(targetKeywords);
-    });
+  it('should fetch and parse an acutal npmjs.com page', function() {
+    // Do we load a page on npm? (#6)
+    // Use the function () format so that we can pass "this"
+    // Set a long timeout so that we can waaaait for third-party data
+    this.timeout(6000);
+    return parser.fetchNPM('https://www.npmjs.com/package/sequelize')
+        .then((result) => {
+            expect(result).toEqual(targetKeywords);
+        });
   });
 });
 
@@ -69,19 +86,59 @@ describe('docker fetcher', () => {
     // Do we convert the official repos list we get into something useful? (#1)
     expect(parser.parseDockers(sampleDocker)).toEqual(targetDocker);
   });
-  it('should fetch and parse actual JSON from the Docker endpoint', function () {
+  it('should fetch and parse actual JSON from the Docker endpoint', function() {
     // Do we pull in official repos? (#1)
-    // Use the function () format so that we can pass this
+    // Use the function () format so that we can pass "this"
+    // Set a long timeout so that we can waaaait for third-party data
     this.timeout(6000);
-    return parser.fetchDockers('https://hub.docker.com/v2/repositories/library/?page_size=3')
-      .then((result) => {
-        expect(result).toEqual(targetDocker);
-      });
+    return parser.fetchDockers('https://hub.docker.com/v2/repositories/library/?page_size=10')
+        .then((result) => {
+            expect(result).toEqual(targetDocker);
+        });
   });
 });
+describe('npm/docker matcher', function () {
+  let stubDockerRepos;
+  let stubDependencies;
+  let stubNPM;
 
-describe('npm/docker matcher', () => {
+  before(() => {
+    // stub out parser.fetchDockers so we don't do a fetch to get
+    // all official Docker repos
+    stubDockerRepos = sinon.stub(parser, 'fetchDockers').returns(Promise.resolve(targetDocker));
 
+    // stub out parser.dependencies so we have known dependencies
+    stubDependencies = sinon.stub(parser, 'dependencies').returns(pjDeps);
+
+    // stub out parser.fetchNPM so that we can have a known list of keywords
+    //  n.b. fetchNPM fetches and parses, so output is the keywords
+    stubNPM = sinon.stub(parser, 'fetchNPM').returns(Promise.resolve(targetKeywords));
+  });
+
+  it('should return the expected list of Docker repos, given a standard input', function (done) {
+    this.timeout(60000);
+    parser.matchDependencies('./test/fixtures/package-test.json')
+      .then(function (data) {
+        console.log('out', data);
+        expect(data).toEqual(targetFinal);
+        done();
+    });
+  });
+
+  // after(() => {
+  //     // un-stub
+  //     // parser.fetchDockers.restore();
+  //     // parser.dependencies.restore();
+  //     // parser.fetchNPM.restore();
+  // });
+
+  /*
+  return parser.matchDependencies('./test/fixtures/package-test.json')
+      .then(function (result) {
+          console.log('result',result);
+          expect(result).toEqual(targetFinal);
+      });
+      */
 });
 
 // Check that we grab all of the official repos from Docker (#1)
